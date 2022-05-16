030528ec350956d7aea2b86f5ba26587c0207082
Orthoceras <florian.berthier@hotmail.com>
2022-05-16 15:30:41 +0200

    feat: creation home

---
+++ src/components/player_card.js
@@ -0,0 +1,7 @@
+function PlayerCard () {
+    <div className="player-card">
+        <img src="/img/Florian_Berthier.jpeg" alt="Florian Berthier"/>
+    </div>
+}
+
+export default PlayerCard
--- src/views/App.js
+++ src/views/App.js
@@ -1,13 +1,14 @@
 import './scss/App.scss';
 import background from '../img/website-development.jpg'
 import Header from '../components/header'
+import PlayerCard from '../components/player_card';
 
 function App() {
   return (
     <>
       <Header />
-      <main style={{ background: `cover url(${background})` }}>
-        
+      <main id='home' style={{ background: `url(${background})` }}>
+        <PlayerCard />   
       </main>
     </>
   );
