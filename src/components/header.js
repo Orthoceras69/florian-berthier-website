0e49e8c2333e53c79f83a3eba33e787fcf628222
Orthoceras <florian.berthier@hotmail.com>
2022-05-16 12:18:16 +0200

    feat: header creation

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,18 @@
 function Header() {
     return (
       <header>
-        <a href="" className="logo">
-            <img src="/img/Logo_Blanc_Nom.png" alt="Logo Florian Berthier"/>
-        </a>
-        <nav>
-            <a href="">Accueil</a>
-            <a href="">À propos</a>
-            <a href="">Compétences</a>
-            <a href="">Projets</a>
-            <a href="">Contact</a>
-        </nav>
+        <div className="header-content">
+            <a href="" className="logo">
+                <img src="/img/Logo_Blanc_Nom.png" alt="Logo Florian Berthier"/>
+            </a>
+            <nav>
+                <a href="">Accueil</a>
+                <a href="">À propos</a>
+                <a href="">Compétences</a>
+                <a href="">Projets</a>
+                <a href="">Contact</a>
+            </nav>
+        </div>
       </header>
     );
   }
--- src/views/App.js
+++ src/views/App.js
@@ -1,10 +1,14 @@
 import './scss/App.scss';
+import background from '../img/website-development.jpg'
 import Header from '../components/header'
 
 function App() {
   return (
     <>
       <Header />
+      <main style={{ background: `cover url(${background})` }}>
+        
+      </main>
     </>
   );
 }
