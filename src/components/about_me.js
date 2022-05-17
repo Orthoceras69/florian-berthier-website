00563209e856cfd534a1960bf36cf796b676aec4
Orthoceras <florian.berthier@hotmail.com>
2022-05-17 16:00:13 +0200

    feat: about me part complete

--- src/components/about_me.js
+++ src/components/about_me.js
@@ -4,39 +4,94 @@ import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 function AboutMe() {
 	return (
 		<aside>
-			<FontAwesomeIcon icon={solid("circle-info")} className="picto" />
+			<FontAwesomeIcon icon={solid("circle-user")} className="picto" />
 			<h2 id="a_propos">À propos</h2>
 			<p>
 				Je m'appelle Florian BERTHIER, je suis Développeur & Intégrateur Web
 				Freelance et également étudiant en alternance à Ynov Campus Lyon en Bachelor
 				Informatique en spécialisation Développement Web et Logiciel.
 			</p>
-			<div className="columns">
+			<div className="columns coordonees">
 				<div className="column1_2">
 					<h3>Mes Coordonnées :</h3>
 					<p>
-						Téléphone : <a href="tel:+33659097732">+33 (0) 6 59 09 77 32</a>
+						Téléphone :{" "}
+						<a href="tel:+33659097732">
+							<i>+33 (0) 6 59 09 77 32</i>
+						</a>
 					</p>
 					<p>
 						Email :{" "}
 						<a href="mailto:florian.berthier@hotmail.com">
-							florian.berthier@hotmail.com
+							<i>florian.berthier@hotmail.com</i>
 						</a>
 					</p>
 					<p>
 						Discord :{" "}
 						<a href="https://www.discordapp.com/users/Orthoceras#6318">
-							Orthoceras#6318
+							<i>Orthoceras#6318</i>
 						</a>
 					</p>
-					<p>Actuellement : Freelance / Alternant</p>
+					<p>
+						Actuellement : <i>Freelance</i> <i>Étudiant</i> <i>Alternant</i>
+					</p>
 				</div>
 				<div className="column1_2">
 					<h3>Mes Centres d'Intérets :</h3>
+					<div className="columns hobbies">
+						<div className="column1_4">
+							<div className="hobby">
+								<FontAwesomeIcon icon={solid("gamepad")} />
+								<strong>Jeux Vidéos</strong>
+							</div>
+						</div>
+						<div className="column1_4">
+							<div className="hobby">
+								<FontAwesomeIcon icon={solid("headphones-simple")} />
+								<strong>Musique</strong>
+							</div>
+						</div>
+						<div className="column1_4">
+							<div className="hobby">
+								<FontAwesomeIcon icon={solid("film")} />
+								<strong>Cinéma</strong>
+							</div>
+						</div>
+						<div className="column1_4">
+							<div className="hobby">
+								<FontAwesomeIcon icon={solid("mug-hot")} />
+								<strong>Café</strong>
+							</div>
+						</div>
+						<div className="column1_4">
+							<div className="hobby">
+								<FontAwesomeIcon icon={solid("medal")} />
+								<strong>Sport</strong>
+							</div>
+						</div>
+						<div className="column1_4">
+							<div className="hobby">
+								<FontAwesomeIcon icon={solid("map-location")} />
+								<strong>Voyages</strong>
+							</div>
+						</div>
+						<div className="column1_4">
+							<div className="hobby">
+								<FontAwesomeIcon icon={solid("book-open")} />
+								<strong>Lecture</strong>
+							</div>
+						</div>
+						<div className="column1_4">
+							<div className="hobby">
+								<FontAwesomeIcon icon={solid("camera-retro")} />
+								<strong>Photographie</strong>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 			<div className="button">
-				<a href="">
+				<a href="/inc/cv_florian_berthier.pdf" download="CV_Florian_Berthier.pdf">
 					Télécharger mon CV <FontAwesomeIcon icon={solid("download")} />
 				</a>
 			</div>
--- src/components/player_card.js
+++ src/components/player_card.js
@@ -6,11 +6,9 @@ function PlayerCard() {
 	return (
 		<main id="home" style={{ background: `url(${background})` }}>
 			<div className="player_card">
-				<img
-					src="/img/Florian_Berthier.jpeg"
-					alt="Florian Berthier"
-					className="profil_picture"
-				/>
+				<div className="profil_picture">
+					<img src="/img/Florian_Berthier.jpeg" alt="Florian Berthier" />
+				</div>
 				<div className="player_info">
 					<h1>
 						<strong>FLORIAN BERTHIER</strong>
@@ -18,7 +16,7 @@ function PlayerCard() {
 						Développeur & Intégrateur Web
 					</h1>
 					<nav className="columns social_nav">
-						<div className="column1_6">
+						<div className="column1_8">
 							<a
 								href="https://www.linkedin.com/in/%E2%98%91-florian-berthier-105ba2179/"
 								className="linkedin"
@@ -26,22 +24,22 @@ function PlayerCard() {
 								<FontAwesomeIcon icon={brands("linkedin-in")} />
 							</a>
 						</div>
-						<div className="column1_6">
+						<div className="column1_8">
 							<a href="https://www.twitch.tv/orthoceras_" className="twitch">
 								<FontAwesomeIcon icon={brands("twitch")} />
 							</a>
 						</div>
-						<div className="column1_6">
+						<div className="column1_8">
 							<a href="https://www.instagram.com/flo_brthr/" className="instagram">
 								<FontAwesomeIcon icon={brands("instagram")} />
 							</a>
 						</div>
-						<div className="column1_6">
+						<div className="column1_8">
 							<a href="https://www.facebook.com/florianbrthr" className="facebook">
 								<FontAwesomeIcon icon={brands("facebook-square")} />
 							</a>
 						</div>
-						<div className="column1_6">
+						<div className="column1_8">
 							<a
 								href="https://www.discordapp.com/users/Orthoceras#6318"
 								className="discord"
@@ -49,9 +47,19 @@ function PlayerCard() {
 								<FontAwesomeIcon icon={brands("discord")} />
 							</a>
 						</div>
-						<div className="column1_6">
+						<div className="column1_8">
+							<a href="https://github.com/Orthoceras69" className="github">
+								<FontAwesomeIcon icon={brands("github")} />
+							</a>
+						</div>
+						<div className="column1_8">
 							<a href="mailto:florian.berthier@hotmail.com" className="envelope">
 								<FontAwesomeIcon icon={solid("envelope")} />
+							</a>
+						</div>
+						<div className="column1_8">
+							<a href="tel:+33659097732" className="phone">
+								<FontAwesomeIcon icon={solid("phone")} />
 							</a>
 						</div>
 					</nav>
