4b4055e9902d85ae82bffa202cfaa9e836922013
Orthoceras <florian.berthier@hotmail.com>
2022-06-16 20:59:39 +0200

    feat: error 404 page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./scss/App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./homepage";
 import ProjectPage from "./projectpage";
+import ErrorPage from "./errorpage";
 
 export default function App() {
 	return (
@@ -9,6 +10,7 @@ export default function App() {
 			<Routes>
 				<Route index path="/" element={<HomePage />} />
 				<Route path="projects" element={<ProjectPage />} />
+				<Route path="*" element={<ErrorPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
